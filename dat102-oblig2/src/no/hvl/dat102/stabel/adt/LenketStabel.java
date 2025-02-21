a05e7d5deda1243963546cfa44f186ee8cc38df8
h676615 <142935129+h676615@users.noreply.github.com>
2025-02-21 12:22:01 +0100

    dd

--- dat102-oblig2/src/no/hvl/dat102/stabel/adt/LenketStabel.java
+++ dat102-oblig2/src/no/hvl/dat102/stabel/adt/LenketStabel.java
@@ -1,5 +1,62 @@
 package no.hvl.dat102.stabel.adt;
 
-public class LenketStabel {
+import java.util.EmptyStackException;
 
+public class LenketStabel<T> implements StabelADT<T> {
+
+	private Node topNode;
+	
+	public LenketStabel() {
+		topNode = null;
+	}
+	
+	@Override
+	public void push(T newEntry) {
+		topNode = new Node(newEntry, topNode);
+	}
+
+	@Override
+	public T pop() {
+		if(topNode == null) {
+			throw new EmptyStackException();
+		}
+		T top = topNode.data;
+		topNode = topNode.neste;
+		return top;
+	}
+
+	@Override
+	public T peek() {
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		} else {
+			return topNode.data;
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return topNode == null;
+	}
+
+	@Override
+	public void clear() {
+		topNode = null;
+
+	}
+
+	private class Node {
+		
+		private T data;
+		private Node neste;
+
+		private Node(T data) {
+			this(data, null);
+		}
+
+		private Node(T data, Node neste) {
+			this.data = data;
+			this.neste = neste;
+		}
+	}
 }
--- dat102-oblig2/src/no/hvl/dat102/stabel/adt/StabelADT.java
+++ dat102-oblig2/src/no/hvl/dat102/stabel/adt/StabelADT.java
@@ -1,5 +1,10 @@
 package no.hvl.dat102.stabel.adt;
 
-public interface StabelADT {
-
+public interface StabelADT <T> {
+	
+	public void push(T newEntry);
+	public T pop();
+	public T peek();
+	public boolean isEmpty();
+	public void clear();
 }
